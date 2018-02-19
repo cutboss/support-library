7df963372b66d707ea8ed0527368d8a379a1d4ca
CUTBOSS <cutboss.appmaster@gmail.com>
2018-02-19 21:13:26 +0900

    feat: StringUtils

---
+++ string/src/main/java/cutboss/support/string/StringUtils.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2018 CUTBOSS
+ */
+
+package cutboss.support.string;
+
+import android.util.Log;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ * StringUtils.
+ *
+ * @author CUTBOSS
+ */
+public class StringUtils {
+    /**
+     *
+     *
+     * @param text
+     * @param charset
+     * @return Bytes
+     */
+    public static int getBytes(String text, String charset) {
+        int length = 0;
+        if (null == text) {
+            return length;
+        }
+        try {
+            length = text.getBytes(charset).length;
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return length;
+    }
+}
