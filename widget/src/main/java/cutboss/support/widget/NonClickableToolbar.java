4c736a2773f0e374477422d9748102b706305605
CUTBOSS <cutboss.appmaster@gmail.com>
2018-09-04 07:28:25 +0900

    feat:

---
+++ widget/src/main/java/cutboss/support/widget/NonClickableToolbar.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2017-2018 CUTBOSS
+ */
+
+package cutboss.support.widget;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * NonClickableToolbar.
+ *
+ * @author CUTBOSS
+ */
+public class NonClickableToolbar extends Toolbar {
+    public NonClickableToolbar(Context context) {
+        super(context);
+    }
+
+    public NonClickableToolbar(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public NonClickableToolbar(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        return performClick();
+    }
+
+    @Override
+    public boolean performClick() {
+        return super.performClick();
+    }
+}
