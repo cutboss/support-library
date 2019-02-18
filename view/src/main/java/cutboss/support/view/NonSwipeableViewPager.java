f288756120018621787b5b03af1b826abe58b069
CUTBOSS <cutboss.appmaster@gmail.com>
2019-02-18 21:56:51 +0900

    feat:

---
+++ view/src/main/java/cutboss/support/view/NonSwipeableViewPager.java
@@ -0,0 +1,36 @@
+package cutboss.support.view;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * ViewPager
+ * (Non Swipeable)
+ */
+public class NonSwipeableViewPager extends ViewPager {
+    public NonSwipeableViewPager(Context context) {
+        super(context);
+    }
+
+    public NonSwipeableViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        performClick();
+        return false;
+    }
+
+    @Override
+    public boolean performClick() {
+        return super.performClick();
+    }
+}
