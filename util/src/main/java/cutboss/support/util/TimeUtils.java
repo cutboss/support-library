b7f76e764a1a4facfe9601adbfac44b5c19b4074
CUTBOSS <cutboss.appmaster@gmail.com>
2019-02-19 18:46:55 +0900

    feat:

---
+++ util/src/main/java/cutboss/support/util/TimeUtils.java
@@ -0,0 +1,9 @@
+package cutboss.support.util;
+
+public class TimeUtils {
+    public static final long SECOND_MILLIS = 1000;
+    public static final long MINUTE_MILLIS = (60 * SECOND_MILLIS);
+    public static final long HOUR_MILLIS = (60 * MINUTE_MILLIS);
+    public static final long DAY_MILLIS = (24 * HOUR_MILLIS);
+    public static final long WEEK_MILLIS = (7 * DAY_MILLIS);
+}
